f2f92358f730ab9dfad85c976b70e68e3abddbad
Artem Dmytrishyn <gamegir@gmail.com>
2022-06-24 21:39:17 +0300

    Updates

---
+++ precache-manifest.e1f1bc555035a5c343e5c2b5bc1b35a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e54753066fc22570815a7a567ea6b8d",
+    "url": "/travelcream-practice/index.html"
+  },
+  {
+    "revision": "b8cbbcf0f8b7b679865e",
+    "url": "/travelcream-practice/static/css/main.f26f7710.chunk.css"
+  },
+  {
+    "revision": "53b491f04ce124bad334",
+    "url": "/travelcream-practice/static/js/2.64a619e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travelcream-practice/static/js/2.64a619e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8cbbcf0f8b7b679865e",
+    "url": "/travelcream-practice/static/js/main.449f2ed0.chunk.js"
+  },
+  {
+    "revision": "baaba4bf319fab7a30d8",
+    "url": "/travelcream-practice/static/js/runtime-main.f53d02e6.js"
+  },
+  {
+    "revision": "34a9b44f6b6f61a200ba425dcf889d04",
+    "url": "/travelcream-practice/static/media/img.34a9b44f.png"
+  }
+]);
